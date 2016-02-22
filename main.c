a7d687e97e115080c097dfd3f8a3d014ce8fa495
laura0918 <laelarmi0918@gmail.com>
2016-02-22 00:22:45 -0500

    2) Versión 2 del fichero, la linea "int x = 5+5;" ha sido agregada

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 int main()
-{int n;
-for(n=1;n<=5;n++)
-    printf("%d\n", n);
+{
+	int n;
+	for(n=1;n<=5;n++)
+    	printf("%d\n", n);
+
+    int x = 5+5;
+
     return 0;
 }
